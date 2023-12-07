f4e74834253f22319560e426bae0c9575e42a78d
II-Paulus-II <paulus.coding@gmail.com>
2023-12-07 18:02:00 +0000

    improved basic css layout

--- js/app.js
+++ js/app.js
@@ -1,2 +1,9 @@
 console.log("Clyker game is werkn sur!");
 
+const hackingButton = document.getElementById("website-hacker");
+
+function hackingDone (event) {
+  console.log("i have been hacked");
+}
+
+hackingButton.addEventListener("click", hackingDone);
